a91c9892128969cbf9a13c40265dacef0ecf9deb
prince chovatiya <princepatle0720@gmail.com>
2025-09-01 23:28:56 +0530

    Update A3P4.C

--- A3P4.C
+++ A3P4.C
@@ -1,12 +1,27 @@
-#include<stdio.h>
-#include<conio.h>
-void main()
-{
-	int n,i,sum=0,count;
-	float avg;
-	printf("enter number :");
-	scanf("%d",n);
-	count=n;
+#include <stdio.h>
+#include <conio.h>
 
-	for(i=1;i<=n;)
+void main()
+ {
+    int n, i, sum = 0, count;
+    float avg;
+    clrscr();
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    count = n; 
+
+    for(i = 1; i <= n; i++) 
+    {
+        sum += i;
+    }
+
+    avg = (float)sum / count;
+
+    printf("Sum = %d\n", sum);
+    printf("Count = %d\n", count);
+    printf("Average = %.2f\n", avg);
+    getch();
+
 }
